1fa43af05eae5f59324d559bcf7258506487f4f7
emanuelaprsouza <e.paula@aluno.ifsp.edu.br>
2023-03-23 10:53:06 -0300

    atualizada

--- Lista3PR2Emanuela/Program.cs
+++ Lista3PR2Emanuela/Program.cs
@@ -146,12 +146,55 @@ namespace Lista3PR2Emanuela
                         case 2:Console.WriteLine("O mês é fevereiro e tem 28 dias,mas se for ano bissexto ele tera 29 dias");
                             break;
 
+                        case 3:Console.WriteLine("O mês é março e tem 31 dias");
+                            break ;
+
+                        case 4:Console.WriteLine("O mês é abril e tem 30 dias");
+                            break;
+
+                        case 5:Console.WriteLine("O mês é maio e tem 31 dias");
+                            break;  
+
+                        case 6:Console.WriteLine("O mês é junho e tem 30 dias");
+                            break;
+
+                         case 7:Console.WriteLine("O mês é julho e tem 31 dias");
+                            break;
+
+                         case 8:Console.WriteLine("O mês é agosto e tem 31 dias");
+                            break;
+
+                         case 9:Console.WriteLine("O mês é setembro e tem 30 dias");
+                            break;
+
+                         case 10:Console.WriteLine("O mês é outubro e tem 31 dias");
+                            break;
+
+                         case 11:Console.WriteLine("O mês é novembro e tem 30 dias");
+                            break;
+
+                         case 12:Console.WriteLine("O mês é dezembro e tem 31 dias");
+                            break;
+
+                         default: Console.WriteLine("Opção Inválida!");
+                            break;
 
                     }
 
                     break;
 
-             case 4:
+             case 4:int n1,n2;
+
+                    Console.WriteLine("Digite o primeiro número: ");
+                    Console.WriteLine("Digite o segundo número: ");
+
+                     Console.WriteLine("=====MENU=====");
+                    Console.WriteLine("+");
+                    Console.WriteLine("-");
+                    Console.WriteLine("*");
+                    Console.WriteLine("/");
+                    Console.WriteLine("");
+                    Console.WriteLine("===============");
            
                     break;
 
